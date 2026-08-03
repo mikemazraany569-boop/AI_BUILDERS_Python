d9c32cf499d31578f15ff336f1027992c201df4f
Your Name <you@example.com>
2026-08-03 18:35:17 +0300

    tuples

---
+++ collection_exercise/tuples.py
@@ -0,0 +1,12 @@
+my_tuple=(1,2,3,4,5)
+print(len(my_tuple))
+
+print(type(my_tuple))
+
+tuple1=(123,"hello",3.4)
+
+print(type(tuple1))
+
+print(tuple1[1])#accessing the second element of the tuple
+
+print(tuple1[-1])#accessing the last element of the tuple
